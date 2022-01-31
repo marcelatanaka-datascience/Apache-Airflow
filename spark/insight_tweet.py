40bf75760d2e740fa605e883e3e8883b53d51c7f
marcelatanaka-datascience <97251510+marcelatanaka-datascience@users.noreply.github.com>
2022-01-31 16:25:04 -0300

    Create insight_tweet.py

---
+++ spark/insight_tweet.py
@@ -0,0 +1,49 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions
+
+
+if __name__=='__main__':
+	spark=SparkSession\
+		.builder\
+		.appName("twitter_insight_tweet")\
+		.getOrCreate()
+
+	tweet=spark.read.json(
+		"/Users/marcela.tanaka/datapipeline/datalake/silver/twitter_aluraonline/tweet"
+
+		)
+
+	alura = tweet\
+		.where("author_id='1566580880'")\
+		.select("author_id", "conversation_id")
+
+	tweet = tweet_alias("tweet")\
+		.join(
+			alura.alias("alura"),
+			[
+				alura.author_id != tweet.author_id,
+				alura.conversation_id == tweet.conversation_id
+			],
+
+			"left join"	
+		)\
+		.withColumn(
+			"alura_conversation",
+			when(col("alura.conversation_id").isNotNull(),1).otherwise(0)
+
+		).withColumn(
+			"reply_alura",
+			when(col("tweet.in_reply_to_user_id") == '1566580880',1).otherwise(0)
+
+		).groupBy(to_date("created_at").alias("created_date"))\
+		.agg(
+			countDistinct("id").alias("n_tweets"),
+			countDistinct("tweet.conversation_id").alias("n_conversation"),
+			sum("alura_conversation").alias("alura_conversation"),
+			sum("reply_alura").alias("reply_alura")
+		).withColumn("weekday", date_format("created_date", "E"))
+
+	tweet.coalesce(1)\
+		.write\
+		.json("/Users/marcela.tanaka/datapipeline/datalake/gold/twitter_insight_tweet ")
+
